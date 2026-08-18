8f1489b9cfeb92a6c9260320b04e8070e61a411f
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:42:39 +0530

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,39 @@
+print("Using break:") 
+
+ 
+
+for i in range(1, 6): 
+
+  if i == 4: 
+
+      break 
+
+  print(i) 
+
+ 
+
+print("Using continue:") 
+
+ 
+
+for i in range(1, 6): 
+
+  if i == 3: 
+
+      continue 
+
+  print(i) 
+
+ 
+
+print("Using pass:") 
+
+ 
+
+for i in range(1, 4): 
+
+  if i == 2: 
+
+      pass 
+
+  print(i) 
