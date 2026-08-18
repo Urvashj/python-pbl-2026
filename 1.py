d5ab8dee06c5f6a5754a216a4bcfb3917a6411c6
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:29:18 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
-print("Hello, Python!")
-print("Python is easy to learn.")
-print("Python is used in Data Science.")
+numbers = [10, 20, 30, 40, 50] 
+
+ 
+
+print("Array elements:", numbers) 
+
+print("First element:", numbers[0]) 
+
+print("Last element:", numbers[-1]) 
+
+print("Number of elements:", len(numbers)) 
