5bd96152b4bb04900f6165614692eb46616acf3b
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:26:03 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,20 @@
+a = 20
+b = 6
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+x = 10
+x += 5
+print("After += :", x)
+
+x -= 3
+print("After -= :", x)
+
+x *= 2
+print("After *= :", x)
