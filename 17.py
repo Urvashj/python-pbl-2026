236a0ed9df6d172ef757c75aaaf622e1f66bffbb
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:40:28 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,25 @@
+marks = 78 
+
+ 
+
+if marks >= 90: 
+
+  grade = "A+" 
+
+elif marks >= 75: 
+
+  grade = "A" 
+
+elif marks >= 60: 
+
+  grade = "B" 
+
+else: 
+
+  grade = "C" 
+
+ 
+
+print("Marks:", marks) 
+
+print("Grade:", grade) 
