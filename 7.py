862865f57304303000594737c7ddf9b8af9f118c
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:27:36 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,12 @@
+numbers = [10, 20, 30, 40]
+
+print("20 in numbers:", 20 in numbers)
+print("50 not in numbers:", 50 not in numbers)
+
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print("a is b:", a is b)
+print("a is c:", a is c)
+print("a is not c:", a is not c)
