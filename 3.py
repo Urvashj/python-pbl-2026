e5645bdec6cd44d6dc65779679694b5bdfdaf659
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:23:25 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,15 @@
+name = "Urvashi"
+age = 20
+percentage = 85.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Percentage:", percentage)
+print("Student:", is_student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(is_student))
