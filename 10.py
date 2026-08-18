87abf9038263ab2585b1ff588e64ea7bdd13aec9
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:36:19 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,11 @@
+numbers = [10, 20, 30, 40, 50] 
+
+ 
+
+print("Array elements:", numbers) 
+
+print("First element:", numbers[0]) 
+
+print("Last element:", numbers[-1]) 
+
+print("Number of elements:", len(numbers)) 
