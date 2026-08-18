b91386033a0dde55caa832e3a3695376408db5f0
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:24:42 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,12 @@
+num1 = "25"
+num2 = "10.5"
+
+integer_value = int(num1)
+float_value = float(num2)
+string_value = str(integer_value)
+
+print("Integer value:", integer_value)
+print("Float value:", float_value)
+print("String value:", string_value)
+
+print("Sum:", integer_value + 5)
