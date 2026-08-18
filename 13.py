d6dde75326ab3d656e4bff29792bbdb7ff9fe8bd
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:38:11 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,15 @@
+set1 = {10, 20, 30, 40} 
+
+set2 = {30, 40, 50, 60} 
+
+ 
+
+print("Set 1:", set1) 
+
+print("Set 2:", set2) 
+
+print("Union:", set1 | set2) 
+
+print("Intersection:", set1 & set2) 
+
+print("Difference:", set1 - set2) 
