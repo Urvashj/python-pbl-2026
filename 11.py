4c836cf45e6cc7e7504a4b580090ba377f0b9e13
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:36:56 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,27 @@
+fruits = ["Apple", "Banana", "Mango"] 
+
+ 
+
+print("Original list:", fruits) 
+
+ 
+
+fruits.append("Orange") 
+
+print("After adding:", fruits) 
+
+ 
+
+fruits[1] = "Grapes" 
+
+print("After changing:", fruits) 
+
+ 
+
+fruits.remove("Apple") 
+
+print("After removing:", fruits) 
+
+ 
+
+print("First fruit:", fruits[0]) 
