d3d58def8104974e284b085e772189bdb7c19b35
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:50:13 +0530

    Add files via upload

---
+++ 29.py
@@ -0,0 +1,13 @@
+import statistics 
+
+ 
+
+numbers = [10, 20, 20, 30, 40] 
+
+ 
+
+print("Mean:", statistics.mean(numbers)) 
+
+print("Median:", statistics.median(numbers)) 
+
+print("Mode:", statistics.mode(numbers)) 
