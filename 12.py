b144ed3b5dc5e360e78f2fa306a710985e211988
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:37:31 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,13 @@
+numbers = (10, 20, 30, 40, 50) 
+
+ 
+
+print("Tuple:", numbers) 
+
+print("First element:", numbers[0]) 
+
+print("Last element:", numbers[-1]) 
+
+print("Length:", len(numbers)) 
+
+print("Count of 20:", numbers.count(20)) 
