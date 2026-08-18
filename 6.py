62b48e385184c2524fcd9d793fdaa73020737b62
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:27:02 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a < b:", a < b)
+print("a > b:", a > b)
+
+x = 15
+
+print("x > 10 and x < 20:", x > 10 and x < 20)
+print("x < 10 or x == 15:", x < 10 or x == 15)
+print("not(x > 10):", not(x > 10))
