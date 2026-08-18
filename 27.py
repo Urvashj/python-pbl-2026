631b89af398af50858f74321f1bed314236e83a7
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:47:59 +0530

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,15 @@
+import math 
+
+ 
+
+number = 25 
+
+ 
+
+print("Square root:", math.sqrt(number)) 
+
+print("Power:", math.pow(2, 3)) 
+
+print("Ceiling:", math.ceil(4.3)) 
+
+print("Floor:", math.floor(4.8)) 
