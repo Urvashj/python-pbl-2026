175c114dcc0c155f8cfa1e4a16cdde18366a5911
Urvashi Makwana <urvashimakawana03@gmail.com>
2026-08-18 22:28:42 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,10 @@
+text = "Python Programming"
+
+print("String:", text)
+print("First character:", text[0])
+print("Last character:", text[-1])
+print("Length:", len(text))
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Replace:", text.replace("Python", "Data"))
+print("Slice:", text[0:6])
